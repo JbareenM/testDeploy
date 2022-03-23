69f30e8b049c69d8bc52ccc8067dea97cb2ef503
Jbareen Mohamad <jbareen.jeme@gmail.com>
2022-03-23 11:01:49 +0200

    deploy changes

--- server/server.ts
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3001;
-
-app.use(express.static('../client/build'));
-
-app.listen(port, ()=>{
-    console.log(`server is running on ${port}`);
-})
