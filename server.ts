20e1f255bfe30e8524a408ad2c798db366013107
JbareenM <44744884+JbareenM@users.noreply.github.com>
2022-03-23 10:54:16 +0200

    Update server.ts

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(express.static('../client/build'));
+app.use(express.static('./client/build'));
 
 app.listen(port, ()=>{
     console.log(`server is running on ${port}`);
